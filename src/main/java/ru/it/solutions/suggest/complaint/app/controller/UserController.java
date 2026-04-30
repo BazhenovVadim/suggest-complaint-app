49d1c332674347491b38afe909f7a7c3ed665eb3
Вадим <Vadimbazhenov74@gmail.com>
2026-04-30 14:13:17 +0300

    access token for vk bot

--- src/main/java/ru/it/solutions/suggest/complaint/app/controller/UserController.java
+++ src/main/java/ru/it/solutions/suggest/complaint/app/controller/UserController.java
@@ -1,5 +1,6 @@
 package ru.it.solutions.suggest.complaint.app.controller;
 
+import io.jsonwebtoken.Jwt;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,13 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.it.solutions.suggest.complaint.app.model.dto.user.UserBotCheckResponseDto;
 import ru.it.solutions.suggest.complaint.app.model.dto.user.UserResponseDto;
 import ru.it.solutions.suggest.complaint.app.service.UserService;
+import ru.it.solutions.suggest.complaint.app.service.auth.JWTService;
 
 @RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
-
+    private final JWTService jwtService;
     @GetMapping("/check")
     public ResponseEntity<UserBotCheckResponseDto> checkUser(@RequestParam(required = false) String vkUserId,
                                                              @RequestParam(required = false) String tgUserId) {
@@ -27,7 +29,7 @@ public class UserController {
                 : userService.existsByTelegramUserId(tgUserId);
 
         String registrationUrl = exists == null ? null : userService.buildRegistrationLink(vkUserId, tgUserId);
-        return ResponseEntity.ok(new UserBotCheckResponseDto(true, registrationUrl, exists));
-        //TODO: возвращать существует или нет, чтобы бот мог понимать что регистрация есть, ан е просто тру возвращать
+        String accessToken = exists == null ? null : jwtService.generateAccessToken(exists.getId(), exists.getEmail());
+        return ResponseEntity.ok(new UserBotCheckResponseDto(true, registrationUrl, exists, accessToken));
     }
 }
--- src/main/java/ru/it/solutions/suggest/complaint/app/model/dto/user/UserBotCheckResponseDto.java
+++ src/main/java/ru/it/solutions/suggest/complaint/app/model/dto/user/UserBotCheckResponseDto.java
@@ -3,5 +3,6 @@ package ru.it.solutions.suggest.complaint.app.model.dto.user;
 public record UserBotCheckResponseDto(
         boolean exists,
         String registrationUrl,
-        UserResponseDto userResponseDto) {
+        UserResponseDto userResponseDto,
+        String accessToken) {
 }
