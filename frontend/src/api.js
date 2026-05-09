93c73d2f78508625e5fb60212b1be1a8b2b9aa23
gleb <ttiigggg9@gmail.com>
2026-05-09 04:18:37 +0300

    -kafka vk bot (worked one time, every other time kafka didn`t want to start) -fixed login page refresh on wrong passworrd -uncommented vk_bot in docker file; updated requirements

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -47,7 +47,9 @@ api.interceptors.response.use(
         localStorage.removeItem('accessTokenExpiresInSeconds');
       }
 
-      window.location.href = "/login";
+      if (window.location.pathname !== '/login') {
+        window.location.href = '/login'; 
+        }
       return Promise.reject(refreshError);
     }
   },
